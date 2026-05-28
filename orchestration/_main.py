eaf523120d0215c28a3ac427298c96ebf5d1e4b2
Abhinabk <abhinabk123@gmail.com>
2026-05-28 04:24:15 +0000

    added prefect orchestration

--- orchestration/_main.py
+++ orchestration/_main.py
@@ -1,4 +1,8 @@
 from dotenv import load_dotenv
+from duckdb import DuckDBPyConnection
+from prefect import flow, task
+from prefect.cache_policies import NO_CACHE
+import requests
 from config.settings import TRAINS_CSV
 from ingestion.etrain.create_session import create_session
 from orchestration.run_ingestion import run_train_ingestion
@@ -7,13 +11,22 @@ from storage.duckdb.init_db import init_bronze_train_metadata
 from storage.readers.load_csv import load_csv
 from tqdm import tqdm 
 load_dotenv() 
-session = create_session()
 
-train_config_path = TRAINS_CSV
-df = load_csv(train_config_path)
-with get_connection() as con: # type: ignore
-    init_bronze_train_metadata(con)
-    for rows in tqdm(df.itertuples(index=False), total=len(df), desc="Ingesting trains"): # type: ignore
-        train_no = rows.number # type: ignore
-        train_name = rows.name  # type: ignore
-        run_train_ingestion(session,con,str(train_no),str(train_name))
+@task(retries=2,retry_delay_seconds=20,cache_policy=NO_CACHE)
+def ingest_train_flow(session:requests.Session,con:DuckDBPyConnection,train_no:str,train_name:str):
+    run_train_ingestion(session,con,str(train_no),str(train_name))
+
+@flow(name="bronze-train-ingestion")
+def ingest_all_trains():
+    train_config_path = TRAINS_CSV
+    session = create_session()
+    df = load_csv(train_config_path)
+    with get_connection() as con: # type: ignore
+        init_bronze_train_metadata(con)
+        for rows in tqdm(df.itertuples(index=False), total=len(df), desc="Ingesting trains"): # type: ignore
+            train_no = rows.number # type: ignore
+            train_name = rows.name  # type: ignore
+            ingest_train_flow(session,con,str(train_no),str(train_name))
+
+if __name__ == "__main__":
+    ingest_all_trains()
