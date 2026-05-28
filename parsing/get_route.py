3c5846261cc53931765a2deb528e80d7771534f8
Abhinabk <abhinabk123@gmail.com>
2026-05-28 15:03:55 +0000

    parsed route order

---
+++ parsing/get_route.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+from storage.readers.load_html import get_station_html
+from bs4 import BeautifulSoup
+
+
+def route(soup:BeautifulSoup)->dict:
+    select = soup.find('select',attrs={'name':'src'})
+    routes = select.find_all('option')
+    result = []
+    order = 1
+    for r in routes:
+        station_code = r.get('value')
+        station_name = r.get_text(strip=True)
+        result.append(
+            {'station_code':station_code,
+             'station_name':station_name,
+             'order':order
+             })
+        order+=1
+    return result
+
+
+if __name__ == "__main__":
+    html = get_station_html('15959')
+    pprint(route(html))
+
+
+
+
+        
+    
