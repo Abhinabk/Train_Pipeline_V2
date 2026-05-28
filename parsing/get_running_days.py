6207c4d8cd6a0c17680f51525d141469445fc0eb
Abhinabk <abhinabk123@gmail.com>
2026-05-28 14:40:01 +0000

    parsed trains running days

--- parsing/get_running_days.py
+++ parsing/get_running_days.py
@@ -1,15 +1,17 @@
-from config.settings import CACHE_DIR
-from storage.object_store.local import save_dataframe_as_csv
 from storage.readers.load_html import get_station_html
 from bs4 import BeautifulSoup
 
 
-def running_days(soup:BeautifulSoup):
-    pass
+def running_days(soup:BeautifulSoup)->list[str]:
+    tr = soup.find('tr',class_='even dborder')
+    td = tr.find('td',class_='nobl').get_text(strip=True)
+    days = td.split(":")[-1].strip(" ")
+    return days.split()
+
 
 if __name__ == "__main__":
     html = get_station_html('15959')
-    running_days(html)
+    print(running_days(html))
 
 
 
--- parsing/get_train_name_no.py
+++ parsing/get_train_name_no.py
@@ -1,13 +1,11 @@
-from config.settings import CACHE_DIR
-from storage.object_store.local import save_dataframe_as_csv
 from storage.readers.load_html import get_station_html
 from bs4 import BeautifulSoup
 
 
-def train_name_no(soup:BeautifulSoup):
+def train_name_no(soup:BeautifulSoup)->str:
     div = soup.find('div',class_='flexRow flexG1')
     span = div.find('span',class_='exp').get_text(strip=True)
-    return span
+    return "-".join(span.split(" "))
 
 if __name__ == "__main__":
     html = get_station_html('15959')
